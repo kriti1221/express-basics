5130b0b27f3d7f83ebef5e753e4ec8c8852dd69e
kriti <kriti.jindal.37@gmail.com>
2023-12-14 18:18:29 +0530

    added headers and status code

--- requests.js
+++ requests.js
@@ -5,11 +5,12 @@ const server = express();
 
 
 server.get('/', (req, res) => {
+    res.set('Content-Type', 'text/plain');
     res.send("welcome to get request");
 });
 
 server.post('/', (req, res) => {
-    res.send("welcome to post request");
+    res.status(201).send("welcome to post request");
 });
 
 server.delete('/', (req, res) => {
@@ -21,6 +22,6 @@ server.put('/', (req, res) => {
 });
 
 
-server.listen(2700, () => {
-    console.log("the server is running on port 2700");
+server.listen(2900, () => {
+    console.log("the server is running on port 2900");
 });
